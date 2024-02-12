1ff787c9918766489d4fc527076ee4fc6abc96e3
Rakesh60 <rakeshhupta5353@gmail.com>
2024-02-12 16:49:59 +0530

    interpolation,template,directives,data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularClass';
+  color1='red';
+  color2='yellow';
+  show=true;
+  title2='Iphone se 2020';
+  price=4500;
+  updateColour(){
+    this.color1='white';
+    this.color2='teal';
+
+  }
 }
